3bc42f54fe10cdb2f57cbc12d477dbd5475b1779
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-09-28 18:30:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,39 @@
-inputBtn.addEventListener("click", function() {
+document.addEventListener("DOMContentLoaded", function() {
+  const inputBtn = document.getElementById("btn");
+  const output = document.getElementById("output");
 
-     const inputNumber = Number(document.getElementById("ip").value)
+  inputBtn.addEventListener("click", function() {
+    const inputNumber = Number(document.getElementById("ip").value);
 
-function delay(value,time){
-       return new Promise((resolve) => {
-             setTimeout(() => {
-                   resolve (value)
-             },time)
-       })
-}
+    function delay(value, time) {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(value);
+        }, time);
+      });
+    }
 
-delay(inputNumber, 2000)
-.then((result) => {
-     output.innerText = `Result: ${result}` 
-     return delay(result * 2, 1000)
-})
-.then((result) => {
-     output.innerText = `Result: ${result}` 
-     return delay(result - 3, 1000)
-})
-.then((result) => {
-     output.innerText = `Result: ${result}` 
-     return delay(result / 2, 1000)
-})
-.then((result) => {
-     output.innerText = `Result: ${result}` 
-     return delay(result + 10, 1000)
-})
-.then((finalResult) => {
-          output.innerText = `Final Result: ${finalResult}` 
-})
-.catch( (e) => output.innerText = `Error: ${e}`)
-})
+    // Start the promise chain
+    delay(inputNumber, 2000)
+      .then((result) => {
+        output.innerText = `Result: ${result}`;
+        return delay(result * 2, 1000);
+      })
+      .then((result) => {
+        output.innerText = `Result: ${result}`;
+        return delay(result - 3, 1000);
+      })
+      .then((result) => {
+        output.innerText = `Result: ${result}`;
+        return delay(result / 2, 1000);
+      })
+      .then((result) => {
+        output.innerText = `Result: ${result}`;
+        return delay(result + 10, 1000);
+      })
+      .then((finalResult) => {
+        output.innerText = `Final Result: ${finalResult}`;
+      })
+      .catch((e) => (output.innerText = `Error: ${e}`));
+  });
+});
